480a4462b2ff88e42359ec2013796440a67be152
NikolaiProgramist <nikluk2005@mail.ru>
2024-02-05 11:32:05 +0300

    initional commit

---
+++ homework.php
@@ -0,0 +1,19 @@
+<?php
+
+print_r('Hello, World!');
+
+// Lesson 5
+
+print_r(3 ** (4 - 2));
+
+// Lesson 6
+
+print_r("- Did Joffrey agree?\n- He did. He also said \"I'm using this \\n\".");
+
+// Lesson 7
+
+print_r((string) 3.04 . ' times');
+
+// Lesson 8
+
+$name = 'anneirB';
---
+++ index.php
@@ -0,0 +1,116 @@
+<?php
+
+echo 'Hello, World!';
+// => Hello, World!
+
+print_r('Hello, World!');
+// => Hello, World!
+
+// Lesson 5
+
+// На следующей строке отступ равен одному табу
+print_r (((8 / 2) + 5) - (-3 / 2));
+
+print_r(((8 / 2) + 5) - (-3 / 2)); // => 10.5
+
+// Lesson 6
+
+//'Hello'
+//'Goodbye'
+//'G'
+//' '
+//''
+
+print_r("Dracarys!");
+
+//print_r('Dragon's mother');
+// PHP Parse error: syntax error, unexpected 's' (T_STRING), expecting ',' or ')'
+
+print_r("Dragon's mother");
+
+// Dragon's mother said "No"
+
+// Экранируется только ", так как в этой ситуации
+// двойные кавычки имеют специальное значение
+print_r("Dragon's mother said \"No\"");
+// => Dragon's mother said "No"
+
+// \ выводится, если после него идет обычный,
+// а не специальный символ
+print_r("Death is \so terribly final");
+// => Death is \so terribly final
+
+print_r("\\");
+// => \
+
+print_r("- Are you hungry?\n- Aaaarrrgh!");
+
+// - Are you hungry?
+// - Aaaarrrgh!
+
+// Мы это не изучали, но вы должны знать правду
+// Ниже код, который возвращает длину строки
+strlen("a");    // 1
+strlen("\n");   // 1 !!!
+strlen("\n\n"); // 2 !!!
+
+// Lesson 7
+
+print_r(5);
+
+print_r(10.234);
+
+print_r(1 + '7'); // => 8
+
+// Примеры немного искусственные
+// В реальности этот механизм становится полезен при использовании переменных
+print_r((string) 5);
+print_r((int) '345');
+
+// Преобразование типов можно использовать внутри составных выражений
+print_r('Это ' . ((string) 5)); // => 'Это 5'
+
+(int) '4';  // 4
+5 + 4;      // 9
+(string) 9; // '9'
+
+// Lesson 8
+
+// greeting - переводится как приветствие
+$greeting = 'Father!';
+print_r($greeting);
+print_r("\n");
+print_r($greeting);
+// => Father!
+// => Father!
+
+// greeting - переводится как приветствие
+$greeting = 'Father!';
+print_r($greeting); // => Father!
+
+$greeting = 'Mother!';
+print_r($greeting); // => Mother!
+
+//print_r($greeting);
+//$greeting = 'Father!';
+// PHP Notice:  Undefined variable: greeting in /private/var/tmp/index.php on line 3
+
+const PI = 3.14;
+print_r(PI); // => 3.14
+
+$str = <<<EOT
+Пример строки,
+охватывающей несколько строк,
+с использованием heredoc-синтаксиса.
+Здесь не нужно экранировать ни одинарные ', ни двойные " кавычки.
+EOT;
+print_r($str);
+// => Пример строки,
+// => охватывающей несколько строк,
+// => с использованием heredoc-синтаксиса.
+// => Здесь не нужно экранировать ни одинарные ', ни двойные " кавычки.
+
+// Lesson 9
+
+$dollarsCount = 50 * 1.25;
+print_r($dollarsCount);

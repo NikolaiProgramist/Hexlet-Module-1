4d2ebe1ad69cc1c301ef8a3ebef6e239e426057a
NikolaiProgramist <nikluk2005@mail.ru>
2024-03-29 13:12:28 +0300

    Course 2 Lesson 15 added.

--- homework.php
+++ homework.php
@@ -615,3 +615,28 @@ function countUniqChars(string $string): int
 
     return $count;
 }
+
+// Lesson 15
+
+function bubbleSort(array $coll): array
+{
+    $size = count($coll);
+
+    do {
+        $swapped = false;
+
+        for ($i = 0; $i < $size - 1; $i++) {
+            if ($coll[$i] > $coll[$i + 1]) {
+                $temp = $coll[$i];
+                $coll[$i] = $coll[$i + 1];
+                $coll[$i + 1] = $temp;
+
+                $swapped = true;
+            }
+        }
+
+        $size--;
+    } while ($swapped);
+
+    return $coll;
+}
